bfcfbe111b0673e3e0a21923112f8c74cb3f41cd
whazami <44798789+whazami@users.noreply.github.com>
2018-11-14 23:55:44 +0100

    Update main.c
    Voici l'exercice 45

--- src/main.c
+++ src/main.c
@@ -2,19 +2,42 @@
 //
 
 #include "my_header.h"
-
 #include "TP_lib.h"
 
-int main(void)
+int main (void)
 {
-	short resultat, i = 0;
-	while(i<10)
-	{
-		resultat = LancerDe();
-		i++;
-	}
-}
+	int a = LancerDe(), P=0;
+						  
+		do
+		{
+		if (a%2==0)
+		{
+		   P = P + a;
+		   a = LancerDe();
+		}
+			
+		else if (a == 3)
+		{
+		   P = P * 2;
+		   a = LancerDe();
+		}
+			   
+	    else
+		{
+			if (P<2)
+			{
+			P = 0;
+			a = LancerDe();
+			}
+			else
+			{
+		    P = P - 2;
+			a = LancerDe();
+			}
+			}
+		}while (a != 1);
 
 return 0;
 
-//Ce programme modélise 10 lancers de dés successifs puisqu'il fait apparaître aléatoirement 10 chiffres compris entre 1 et 6.
+}
+
