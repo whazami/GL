fcc6bad34b2d5221fb28c8942828318a0f7fc9e4
whazami <44798789+whazami@users.noreply.github.com>
2018-11-14 22:31:19 +0100

    Update main.c
    Voici l'exercice 43

--- src/main.c
+++ src/main.c
@@ -3,7 +3,39 @@
 
 #include "my_header.h"
 
+#include "stdlib.h"
+
 int main(void)
 {
-    return 0;
+	
+	long int S1 = 0, S4 = 0;
+	int n = 0, v = 1, r, S3 = rand()%10 + 1, p=0, m=1;
+
+	while (n<3292)
+	{
+		S1 = S1+3*n;
+		n = n+1;
+	}
+			 
+	while (v<12)
+	{
+		r = rand()%10 + 1;
+		S3 = S3 + r;
+		v = v+1;
+	}
+	
+	while (p<20)
+	{
+		if (m%3==0 || m%7==0)
+		{
+			S4 = S4 + m;
+		    p = p+1;
+		    m = m+1;
+		}
+		
+	    else
+			m = m+1;
+		}
+	
+return 0;
 }
