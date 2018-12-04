152e486d3c10caf5559ba1df499e5ae57d614345
whazami <44798789+whazami@users.noreply.github.com>
2018-12-04 23:25:36 +0100

    J'ai fini l'exercice 82
    On indiquera à l'utilisateur que s'il souhaite mettre un chiffre dans une coordonnée, il devra préciser que le chiffre des dizaines est un 0.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include "userint.h"
+#include "stdio.h"
 
-#include "my_header.h"
-
-int main(void)
+int main (void)
 {
-    return 0;
+	char ligne1[80] =  "Nom: pentagone_B", nom_fig[100], message[100];
+	char ligne2[80] =  "Nb points: 5"; 
+	char ligne3[80] =  "Coordonnees: 10 20 30 20 30 40 43 53 35 65";
+	long x[5], y[5], nb_pts;
+	
+	if(ligne3[41]==0 || ligne2[11]==0 || ligne1[5]==0)
+		MessagePopup("Erreur", "Il manque au moins une donnée.");
+	
+	else
+	{
+    sscanf(ligne1,"%*s %s",nom_fig);
+	sscanf(ligne2,"%*s %*s %d",&nb_pts);
+	sscanf(ligne3,"%*s %d %*d %d %*d %d %*d %d %*d %d %*d",&x[0], &x[1], &x[2], &x[3], &x[4]);
+	sscanf(ligne3,"%*s %*d %d %*d %d %*d %d %*d %d %*d %d",&y[0], &y[1], &y[2], &y[3], &y[4]);
+	
+	sprintf(message,"La figure %s possède %d sommets",nom_fig,nb_pts);
+	MessagePopup("informations figure",message);
+	}
+return 0;
 }
